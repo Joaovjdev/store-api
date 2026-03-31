f3bad8c9d5ec6f7965fe996cc9b1b239f9764663
Joaovjdev <zumbe25tt@gmail.com>
2026-03-30 22:01:39 -0300

    feat: update findByEmail method to exclude password and select specific fields

--- src/repository/auth.repository.ts
+++ src/repository/auth.repository.ts
@@ -3,8 +3,18 @@ import type { Auth } from "../entity/auth.entity.js";
 import { prisma } from "../config/database.js";
 
 export class AuthRepository implements IAuthRepository {
-    async findByEmail(email: string): Promise<Auth | null> {
-        const auth = await prisma.auth.findUnique({ where: { email } });
+    async findByEmail(email: string): Promise<Omit<Auth, "password"> | null> {
+        const auth = await prisma.auth.findUnique({
+            where: { email },
+            select: {
+                id: true,
+                email: true,
+                username: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
+
         return auth;
     }
 }
